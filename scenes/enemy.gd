47e0ef34f4927d9b2ebd2c535bb15f5cad4b9867
Hoetty <kilian.behnke@outlook.com>
2025-07-30 21:42:22 +0200

    Spawning

---
+++ scenes/enemy.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+const SPEED = 50.0
+
+
+func _physics_process(delta: float) -> void:
+    var player = get_tree().get_first_node_in_group("player")
+    
+    velocity = position.direction_to(player.position) * SPEED
+    
+    move_and_slide()
