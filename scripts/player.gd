275acfdd3567f4c3bcf060d19c0d9ad8ffca302f
Kl3XY <gaffke.martin@outlook.de>
2025-07-30 22:37:20 +0200

    Indentation fix, oops

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,9 +3,9 @@ extends CharacterBody2D
 const SPEED = 80.0
 
 enum CharacterType {
-	Wizard,
-	Archer,
-	Warrior
+    Wizard,
+    Archer,
+    Warrior
 }
 
 @onready var sprite = $AnimatedSprite2D
@@ -24,31 +24,31 @@ func cycle():
     sprite.stop()
 
 func get_animation() -> String:
-	match self.character_type:
-		CharacterType.Wizard:
-			return "wizard"
-		CharacterType.Warrior:
-			return "warrior"
-		CharacterType.Archer:
-			return "archer"
-			
-	return "default"
-	
+    match self.character_type:
+        CharacterType.Wizard:
+            return "wizard"
+        CharacterType.Warrior:
+            return "warrior"
+        CharacterType.Archer:
+            return "archer"
+            
+    return "default"
+    
 func _ready() -> void:
-	sprite.play(self.get_animation())
-	sprite.stop()
+    sprite.play(self.get_animation())
+    sprite.stop()
 
 func _physics_process(delta: float) -> void:
-	var direction := Input.get_vector("left", "right", "up", "down")
-	if direction != Vector2.ZERO:
-		velocity = direction * SPEED
-		sprite.play(self.get_animation())
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
-		sprite.stop()
+    var direction := Input.get_vector("left", "right", "up", "down")
+    if direction != Vector2.ZERO:
+        velocity = direction * SPEED
+        sprite.play(self.get_animation())
+    else:
+        velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+        sprite.stop()
 
-	if velocity.x != 0:
-		sprite.flip_h = velocity.x > 0
+    if velocity.x != 0:
+        sprite.flip_h = velocity.x > 0
 
     move_and_slide()
     
