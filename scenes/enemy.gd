5289a0d89f551d597e1234cdecd386f7ac816f93
Hoetty <kilian.behnke@outlook.com>
2025-07-31 21:12:59 +0200

    Fix Spawner

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -5,8 +5,8 @@ const SPEED = 50.0
 
 
 func _physics_process(delta: float) -> void:
-	var player = get_tree().get_first_node_in_group("player")
-	
-	velocity = position.direction_to(player.position) * SPEED
-	
-	move_and_slide()
+    var player = get_tree().get_first_node_in_group("player")
+    
+    velocity = position.direction_to(player.position) * SPEED
+    
+    move_and_slide()
