eee6efaa12ca66845175ccdd5af351e1b2f0cd26
Kl3XY <gaffke.martin@outlook.de>
2025-07-31 21:56:20 +0200

    Attack rework damage numbaas

--- scenes/Projectiles/Basic Mage Wand/basic_mage_wand_projectile.gd
+++ scenes/Projectiles/Basic Mage Wand/basic_mage_wand_projectile.gd
@@ -1,9 +1,14 @@
 extends Projectile
 
+const DAMAGE_NUMBER = preload("res://scenes/UI/DamageNumber.tscn")
+
 func _on_damage_area_body_entered(body: Node2D):
     if body is Enemy:
         body.queue_free();
         queue_free();
+        var dmgNumInst = DAMAGE_NUMBER.instantiate();
+        dmgNumInst.position = body.position;
+        get_tree().current_scene.add_child(dmgNumInst);
 
 func _process(delta: float) -> void:
     rotation = direction.angle();
