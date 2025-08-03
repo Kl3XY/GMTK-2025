42b224d6230c9c722461381b7507a4a99648927f
Hoetty <kilian.behnke@outlook.com>
2025-08-03 15:42:07 +0200

    Web Export

--- scenes/options_menu.gd
+++ scenes/options_menu.gd
@@ -6,8 +6,6 @@ const HANDLER = preload("res://Achievements/Handler.tres")
 var PLAYER_COLLECTED_GEMS = preload("res://Statistics/Statistics/Stats/Resources/player_collected_gems.tres")
 func _ready() -> void:
     var bus = AudioServer.get_bus_index("Master")
-    if $"../Shop/TextureRect/Label" != null:
-        $"../Shop/TextureRect/Label".text = str(PLAYER_COLLECTED_GEMS.gems) + "x"
 
 func _on_texture_button_pressed() -> void:
     print("optiones")
