b68b51bc68e11aaee27ff1d33db941786b80ef76
Hoetty <kilian.behnke@outlook.com>
2025-08-03 17:16:24 +0200

    Code polish

--- scenes/achievements_menu.gd
+++ scenes/achievements_menu.gd
@@ -8,19 +8,18 @@ func _process(delta: float) -> void:
     _achievement_menu_process(delta);
 
 func _achievement_menu_process(delta: float):
-    if visible == false:
+    if !visible:
         reload_info = true;
-    else:
-        if reload_info == true:
-            reload_info = false;
-            var str = "";
-            for achievement in HANDLER.Achievements:
-                if achievement.hasUnlocked == true:
-                    str += achievement.name + " - " + achievement.short_desc;
-                else:
-                    str += "[color=darkgray]" + achievement.name + " - " + achievement.short_desc + "[/color]"
-                str += "\n"
-            $AchievementList.text = str
+    elif reload_info:
+        reload_info = false;
+        var str = "";
+        for achievement in HANDLER.Achievements:
+            if achievement.hasUnlocked == true:
+                str += achievement.name + " - " + achievement.short_desc;
+            else:
+                str += "[color=darkgray]" + achievement.name + " - " + achievement.short_desc + "[/color]"
+            str += "\n"
+        $AchievementList.text = str
             
 func _on_texture_button_pressed() -> void:
     visible = false;
